c0cd6e352829831fea4bd3c1afa91a024723a504
Christian Nally <sticksallison@gmail.com>
2022-07-12 16:48:35 -0700

    multiple launch sequences to choose from

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.set('view engine','ejs');
 
 // https://spaceflight101.com/falcon-9-ft-countdown-timeline/
 
-const script = [
+const scripts = [
+  [ // Falcon-9 Launch Milestones
   {time: 60, statement:	'Flight Computer to start up.'},
   {time: 50, statement:	'Stage 1, Stage 2 Pressurization for Flight.'},
   {time: 45, statement:	'Launch Director: Go for Launch.'},
@@ -17,22 +18,22 @@ const script = [
   {time: 07, statement:	'Pad Deck Water Deluge System Activation.'},
   {time: 03, statement:	'Merlin Engine Ignishun.'},
 //  {time: 00, statement:	'Strongback Kickback at Liftoff'},
-  {time: 00, statement:	'LIFTOFF!'},
+  {time: 0, statement:	'LIFTOFF!'},
+],
+[ // Countdown to LHL Lecture
+  {time: 100, statement: 'Verify Scheduled Lecture'},
+  {time: 90, statement: 'Publish Lecture Preamble'},
+  {time: 80, statement: 'Prepare Code'},
+  {time: 70, statement: 'Open Code in Multiple VS Code Windows'},
+  {time: 60, statement: 'Create and Join Zoom Meeting'},
+  {time: 50, statement: 'Share Desktop'},
+  {time: 40, statement: 'Enable Automatic Transcripts'},
+  {time: 30, statement: 'Turn Focus Mode On'},
+  {time: 20, statement: 'Open and Arrange Zoom Windows'},
+  {time: 10, statement: 'Share Zoom Link with Cohort on Discord lecture links channel for the appropriate cohort'},
+  {time: 0, statement:	'Action!'},
+]
 ];
-
-// Countdown to LHL Lecture
-// const script = [
-// {time: 100, statement: 'Verify Scheduled Lecture'},
-// {time: 90, statement: 'Publish Lecture Preamble'},
-// {time: 80, statement: 'Prepare Code'},
-// {time: 70, statement: 'Open Code in Multiple VS Code Windows'},
-// {time: 60, statement: 'Create and Join Zoom Meeting'},
-// {time: 50, statement: 'Share Desktop'},
-// {time: 40, statement: 'Enable Automatic Transcript'},
-// {time: 30, statement: 'Turn Focus Mode On'},
-// {time: 20, statement: 'Open and Arrange Zoom Windows'},
-// {time: 10, statement: 'Share Zoom Link with Cohort on Discord lecture links channel for the appropriate cohort'},
-// ];
 
 //
 // Middleware
@@ -45,14 +46,31 @@ app.use(express.urlencoded({extended: false}));
 
 // BROWSE
 app.get('/',(req,res) => {
-  // res.send('home');
-  console.log('JSON.stringify(script)',JSON.stringify(script));
-  const totalDuration = 60;
-  script.forEach(element => {
+  res.render('home');
+});
+
+function getEarliestTime(script){
+  let max = 0;
+  for (let ii = 0; ii < script.length; ii++) {
+    if (script[ii].time > max) {
+      max = script[ii].time;
+    }
+  }
+  return max;
+}
+
+// LAUNCH
+app.post('/launch/start', (req,res) => {
+  const launchId = req.body.launchid;
+  console.log('JSON.stringify(scripts)',JSON.stringify(scripts[launchId]));
+  const totalDuration = getEarliestTime(scripts[launchId]);
+  scripts[launchId].forEach(element => {
     const secondsToGo = totalDuration - element.time;
     setTimeout(()=>{
+      let text = '';
+      text = `Tee Minus ${element.time} seconds: ${element.statement}`;
       say.speak(
-        `Tee Minus ${element.time} seconds: ${element.statement}`,
+        text,
         'Samantha',
         1.0,
         (err) => {
@@ -62,12 +80,29 @@ app.get('/',(req,res) => {
           console.log(element.statement);
         }
       );
-    },(secondsToGo)*1000);    
+    },(secondsToGo)*1000);
   });
+  for (let countDown = 5; countDown > 0; countDown--) {
+    setTimeout(()=>{
+      let text = '';
+      text = `${countDown}`;
+      say.speak(
+        text,
+        'Alex',
+        1.0,
+        (err) => {
+          if (err) {
+            return console.error(err);
+          }
+          console.log('countDown', countDown);
+        }
+      );
+    },(totalDuration - countDown)*1000);
+  }
   const templateVars = {
-    script: script,
+    script: scripts[launchId],
   };
-  res.render('home',templateVars);
+  res.render('launch',templateVars);
 });
 
 // ADD
@@ -77,7 +112,7 @@ app.get('/milestone/new', (req,res) => {
 
 app.post('/milestone/new', (req,res) => {
   console.log('req.body',req.body);
-  script.push({time: req.body.time, statement: req.body.statement});
+  scripts.push({time: req.body.time, statement: req.body.statement});
   res.redirect('/');
 });
 
@@ -85,13 +120,13 @@ app.post('/milestone/new', (req,res) => {
 app.get('/milestone/edit/:index', (req,res) => {
   console.log('req.params',req.params);
   const index = parseInt(req.params.index);
-  console.log('script',script);
+  console.log('scripts',scripts);
   console.log('index',index);
-  console.log('script[index]',script[index]);
+  console.log('scripts[index]',scripts[index]);
   const templateVars = {
     index: index,
-    time: script[index].time,
-    statement: script[index].statement
+    time: scripts[index].time,
+    statement: scripts[index].statement
   };
   res.render('edit', templateVars);
 });
@@ -100,10 +135,10 @@ app.post('/milestone/edit/:index', (req,res) => {
   console.log('req.body',req.body);
   console.log('req.params',req.params);
   const index = parseInt(req.params.index);
-//  script.push({time: req.body.time, statement: req.body.statement});
+//  scripts.push({time: req.body.time, statement: req.body.statement});
 
-  script[index].time = req.body.time;
-  script[index].statement = req.body.statement;
+  scripts[index].time = req.body.time;
+  scripts[index].statement = req.body.statement;
 
   res.redirect('/');
 });
@@ -112,16 +147,16 @@ app.post('/milestone/edit/:index', (req,res) => {
 app.get('/milestone/delete/:index', (req,res) => {
   console.log('req.params',req.params);
   const index = parseInt(req.params.index);
-  console.log('script before the delete',script);
+  console.log('scripts before the delete',scripts);
   console.log('index',index);
-  console.log('script[index]',script[index]);
+  console.log('scripts[index]',scripts[index]);
 
-//  delete script[index];
-//  let item = array.indexOf(script[index]);
+//  delete scripts[index];
+//  let item = array.indexOf(scripts[index]);
   let deleteCount = 1;
-  script.splice(index, deleteCount)
+  scripts.splice(index, deleteCount)
 
-  console.log('script AFTER the delete',script);
+  console.log('scripts AFTER the delete',scripts);
 
   res.redirect('/');
 });
